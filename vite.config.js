88f506b43d92240604ce31aefeeb27910b77e269
1x5 <o17s@icloud.com>
2025-10-29 00:25:50 +0300

    Update vite config for custom domain

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/-2/', // Путь к репозиторию на GitHub Pages (имя репозитория)
+  base: '/', // Корневой путь для custom domain
 })
 
