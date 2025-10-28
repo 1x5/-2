31eb00db56a53dce8b092948a94bab5b2c459735
1x5 <o17s@icloud.com>
2025-10-28 22:52:20 +0300

    Fix Supabase URL typo

--- src/supabase.js
+++ src/supabase.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
 // Ваши данные из Supabase
-const supabaseUrl = 'https://orrkwxgfoafyenudoqh.supabase.co'
+const supabaseUrl = 'https://orrkwxgfoafyenudouqh.supabase.co'
 const supabaseKey = 'sb_publishable_3qDjAT2bYbUcNC9DkkYXEQ_QgkdPvpB'
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
