29e3d3ad9638bf0c66e51ab0ffabb4fb016eea8f
1x5 <o17s@icloud.com>
2025-10-28 22:51:27 +0300

    Fix Supabase credentials

--- src/supabase.js
+++ src/supabase.js
@@ -1,10 +1,8 @@
 import { createClient } from '@supabase/supabase-js'
 
-// Вставьте сюда ваши данные из Supabase
-// 1. Settings → API → Project URL (должен начинаться с https://)
-// 2. Settings → API → anon public key (должен быть длинный ключ)
+// Ваши данные из Supabase
 const supabaseUrl = 'https://orrkwxgfoafyenudoqh.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9ycmt3eGdmb2FmeWVudWRvdXFoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjE2NzY5MzMsImV4cCI6MjA3NzI1MjkzM30.-nFsID7JgtAKbe6zWkAa22qS23jAIlVXzeo4autujsc'
+const supabaseKey = 'sb_publishable_3qDjAT2bYbUcNC9DkkYXEQ_QgkdPvpB'
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
 
